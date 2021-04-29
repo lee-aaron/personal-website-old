0a467df579eb4dc9e6f8a134f2952f4119812013
lee-aaron <lee.aaron.68@gmail.com>
2021-04-28 23:56:16 -0700

    Fix ACL and importmap

--- root-config/server.js
+++ root-config/server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 8080;
 express()
   .use(express.static(path.join(__dirname, 'dist')))
   .get("*", (req, res, next) => {
-    res.sendFile("index.html", { root: "dist"});
+    res.sendFile("index.html", { root: path.join(__dirname, 'dist')});
     next();
   })
   .listen(PORT, () => {
     console.log("Express server listening on port: " + PORT);
-  })
+  });
