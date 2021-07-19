4977ed6d6621781d66abab8837a5db77ae530393
lee-aaron <lee.aaron.68@gmail.com>
2021-07-18 17:52:24 -0700

    Update home page

--- home/src/root.component.tsx
+++ home/src/root.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ServiceNow from './logos/servicenow.svg';
 import EverydayRobot from './logos/everyday_robot.svg';
 import MemVerge from './logos/memVerge.png';
+import DigitalOcean from './logos/DO_Logo_Vertical_Blue.png';
 import "./Home.scss";
 
 export default function Root(props: any) {
@@ -15,6 +16,15 @@ export default function Root(props: any) {
       <section className="highlights">
         <h4>Experience Highlights</h4>
         <div className="description">
+          <article>
+            <h4>Digital Ocean</h4>
+            <img width="100" src={DigitalOcean} alt="Digital Ocean Logo" />
+            <p>
+              I interned at this company in Summer 2021. I primarily worked on the development of the internal admin dashboard for 
+              the Container Registry product to help engineers identify possible issues in customer cases. I learned how to use Docker,
+              Kubernetes, and golang during this internship.
+            </p>
+          </article>
           <article>
             <h4>The Everyday Robot Project</h4>
             <img width="50" src={EverydayRobot} alt="Everyday Robot Project logo" />
